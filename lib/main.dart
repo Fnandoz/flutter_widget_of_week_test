2a3083dbd002ad5eb49c0e4dbf137acfec866992
Fernando Gomes <lfgsnando@gmail.com>
2021-04-30 22:03:01 -0300

    Route to SwitchListTile Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_of_week/animated_container.dart';
 import 'package:flutter_widget_of_week/expanse_panel_test.dart';
+import 'package:flutter_widget_of_week/switch_list_tile.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -44,11 +45,18 @@ class MyApp extends StatelessWidget {
                   applicationVersion: "1.0.0",
                   applicationIcon: FlutterLogo(),
                   applicationLegalese: "Test test test",
-
-
                 );
               },
             ),
+            ListTile(
+              title: Text("Switch List Tile Example"),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SwitchListTileExample()),
+                );
+              },
+            )
           ],
         ),
       ),
